2ccc330a3c9c7952fcb857ec189deec489140a0d
anthonyvallee <tony.vallee@hotmail.com>
2015-02-02 22:29:35 -0500

    DynamicArray Header and Source File
    The basic outline of the DynamicArray class' header and source file were
    written. No logic was written at this point.

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,37 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray()
+{
+	DynamicArray(DEFAULT_CAPACITE);
+}
+
+DynamicArray::DynamicArray(unsigned int _capacite)
+{
+	this->tabElements = new int[_capacite];
+	this->capacite = _capacite;
+}
+
+DynamicArray::~DynamicArray()
+{
+	delete[] tabElements;
+}
+
+int DynamicArray::getElement(unsigned _index)
+{
+	return *tabElements[_index];
+}
+
+void DynamicArray::setElement(unsigned _index, int _valeur)
+{
+
+}
+
+unsigned int DynamicArray::getCapacite()
+{
+	return capacite;
+}
+
+void DynamicArray::setCapacite(unsigned _capacite)
+{
+	this->capacite = _capacite;
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -1,17 +1,21 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
+const unsigned int DEFAULT_CAPACITE = 100;
 
 class DynamicArray
 {
-
 public:
-
-
-
+	DynamicArray();
+	DynamicArray(unsigned int _capacite);
+	~DynamicArray();
+	int getElement(unsigned int _index);
+	void setElement(unsigned int _index, int _valeur);
+	unsigned int getCapacite();
+	void setCapacite(unsigned int _capacite);
 private:
-
-
+	int* tabElements;
+	unsigned int capacite;
 };
 
 
