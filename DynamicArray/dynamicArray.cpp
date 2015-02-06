6294fae34206f9692a3ac6dc15d8aaae4aec2a82
anthonyvallee <tony.vallee@hotmail.com>
2015-02-06 13:27:35 -0500

    Operators overrided and copy constructor
    The operators and copy constructor have been implemented. Overall, two
    tests still fail. Will have to modify code.

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,22 +1,65 @@
 #include "dynamicArray.h"
 
 
-DynamicArray::DynamicArray()
-{
-	this->tabElements = new int[DEFAULT_CAPACITE];
-	this->capacite = DEFAULT_CAPACITE;
-	initArray();
-}
-
 DynamicArray::DynamicArray(unsigned int _capacite)
 {
-	if (_capacite < 1)
-	{
-		throw std::invalid_argument("The given capacity must be greater than or equal to 1.\n");
-	}
+	if (_capacite < 1) throw std::invalid_argument("The given capacity must be greater than or equal to 1.\n");
 	this->tabElements = new int[_capacite];
 	this->capacite = _capacite;
 	initArray();		
+}
+
+DynamicArray::DynamicArray(const DynamicArray& _source)
+	: tabElements(new int[_source.capacite]), capacite(_source.capacite)
+{
+	if (&_source == nullptr) throw std::invalid_argument("Le paramètre passé ne peut être NULL.\n");
+	for (unsigned int i = 0; i < capacite; ++i)
+	{
+		this->tabElements[i] = _source.tabElements[i];
+	}
+}
+
+DynamicArray& DynamicArray::operator=(const DynamicArray& _source)
+{
+	if (&_source == nullptr) throw std::invalid_argument("Le paramètre passé ne peut être NULL.\n");
+	if (&_source != this)
+	{
+		delete[] tabElements;
+		this->capacite = _source.capacite;
+		this->tabElements = new int[capacite];
+		for (unsigned int i = 0; i < capacite; ++i)
+		{
+			tabElements[i] = _source.tabElements[i];
+		}
+	}
+	return *this;
+}
+
+bool DynamicArray::operator==(const DynamicArray& _rhs) const
+{
+	if (&_rhs == nullptr) throw std::invalid_argument("Le paramètre passé ne peut être NULL.\n");
+	if (this->capacite != _rhs.capacite)
+	{
+		return false;
+	}
+	unsigned int smallestArrayLength = std::min(this->capacite, _rhs.capacite);
+	for (int i = 0; i < smallestArrayLength; ++i)
+	{
+		if (this->tabElements[i] != _rhs.tabElements[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+DynamicArray& DynamicArray::operator+=(const DynamicArray& _rhs)
+{
+	if (&_rhs == nullptr) throw std::invalid_argument("Le paramètre passé ne peut être NULL.\n");
+	if (_rhs.capacite >= 1)
+	{
+		return appendArray(*this, _rhs);
+	}
 }
 
 DynamicArray::~DynamicArray()
@@ -26,10 +69,7 @@ DynamicArray::~DynamicArray()
 
 int DynamicArray::getElement(unsigned int _index)
 {
-	if (_index + 1 > capacite)
-	{
-		throw std::out_of_range("The given index is not part of the array.\n");
-	}
+	if (_index + 1 > capacite) throw std::out_of_range("The given index is not part of the array.\n");
 	return tabElements[_index];
 }
 
@@ -49,36 +89,44 @@ unsigned int DynamicArray::getCapacite()
 
 void DynamicArray::setCapacite(unsigned int _capacite)
 {
-	if (_capacite >= 1)
+	if (_capacite <= 0) throw std::invalid_argument("The resize capacity must be greater than or equal to 1.");
+
+	int* newArray = new int[_capacite];
+	int* arrayToRemove = tabElements;
+	for (unsigned int i = 0; i < capacite && i < _capacite; ++i)
 	{
-		int* newArray = new int[_capacite];
-		int* arrayToRemove = tabElements;
-		for (int i = 0; i < capacite && i < _capacite; ++i)
-		{
-			newArray[i] = tabElements[i];
-			std::string tempValue = std::to_string(tabElements[i]);
-		}
-		if (capacite < _capacite)
-		{
-			for (int i = capacite; i < _capacite; ++i)
-			{
-				newArray[i] = 0;
-			}
-		}
-		this->capacite = _capacite;
-		tabElements = newArray;
-		delete arrayToRemove;
+		newArray[i] = tabElements[i];
 	}
-	else
+	if (capacite < _capacite)
 	{
-		throw std::invalid_argument("The resize capacity must be greater than or equal to 1.");
+		for (unsigned int i = capacite; i < _capacite; ++i)
+		{
+			newArray[i] = 0;
+		}
 	}
+	this->capacite = _capacite;
+	tabElements = newArray;
+	delete arrayToRemove;
 }
 
 void DynamicArray::initArray()
 {
-	for (int i = 0; i < capacite; ++i)
+	for (unsigned int i = 0; i < capacite; ++i)
 	{
 		tabElements[i] = 0;
 	}
+}
+
+DynamicArray& DynamicArray::appendArray(const DynamicArray& firstArray, const DynamicArray& secondArray) const
+{
+	DynamicArray appendedArray(firstArray.capacite + secondArray.capacite);
+	for (int i = 0; i < this->capacite; ++i)
+	{
+		appendedArray.tabElements[i] = firstArray.tabElements[i];
+	}
+	for (int i = 0; i < secondArray.capacite; ++i)
+	{
+		appendedArray.tabElements[i + firstArray.capacite] = secondArray.tabElements[i];
+	}
+	return appendedArray;
 }
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -3,15 +3,21 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 const unsigned int DEFAULT_CAPACITE = 100;
 
 class DynamicArray
 {
 public:
-	DynamicArray();
-	DynamicArray(unsigned int _capacite);
+	DynamicArray(unsigned int _capacite = 100);
+	DynamicArray(const DynamicArray & _source);
 	~DynamicArray();
+
+	DynamicArray& operator=(const DynamicArray & _source);
+	bool operator==(const DynamicArray& _rhs) const;
+	DynamicArray& operator+=(const DynamicArray& _rhs);
+
 	int getElement(unsigned int _index);
 	void setElement(unsigned int _index, int _valeur);
 	unsigned int getCapacite();
@@ -20,6 +26,7 @@ private:
 	int* tabElements;
 	unsigned int capacite;
 	void initArray();
+	DynamicArray& appendArray(const DynamicArray& firstArray, const DynamicArray& secondArray) const;
 };
 
 
